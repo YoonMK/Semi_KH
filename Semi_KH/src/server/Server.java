9723bf5e4225b396ef69f1de63c36866784c536f
junc <junc@DESKTOP-DRHDR37>
2016-11-08 12:52:23 +0900

    jk

--- Semi_KH/src/server/Server.java
+++ Semi_KH/src/server/Server.java
@@ -14,88 +14,88 @@ import java.util.HashMap;
 import java.util.Iterator;
 
 public class Server {
-	HashMap<String, DataOutputStream>clients ;
-	
-	public Server() {
-		
-		clients =new HashMap();
-		Collections.synchronizedMap(clients);
-		SimpleDateFormat sf = new SimpleDateFormat();
-		
-		try {
-			ServerSocket server = new ServerSocket(7777);
-			System.out.println("["+sf.format(new Date())+"]"+"서버 시작");
-			
-			while(true)
-			{
-				Socket client = server.accept();
-				System.out.println("["+sf.format(new Date())+"]"+
-						"["+"로그인"+":"+client.getPort()+"] 에서 접속");
-				
-				new TCPMulServerReceiver(client).start();
-			}
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	class TCPMulServerReceiver extends Thread
-	{
-		String name;
-		DataOutputStream output;
-		DataInputStream input;
-		public TCPMulServerReceiver(Socket client) {
-			try {
-				name = "["+client.getInetAddress()+"]";
-				output = new DataOutputStream(client.getOutputStream());
-				input = new DataInputStream(client.getInputStream());
-					
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		
-		@Override
-		public void run() {
-			
-			try {
-				name = input.readUTF();
-				sendToAll("#"+name+"님이 입장하였습니다.");
-				clients.put(name, output);
-				
-				System.out.println("현재접속자수:"+clients.size());
-				
-				while(input!=null)
-				{
-					sendToAll(input.readUTF());
-					System.out.println(input.readUTF());
-				}
-			} catch (IOException e) {
-				clients.remove(name);
-				sendToAll("#"+name+"님이 퇴장하셨습니다.");
-				System.out.println("현재접속자수:"+clients.size());
-			}
-		
-		}// run 종료
-	}
-	void sendToAll(String msg)
-	{
-		Iterator it =clients.keySet().iterator();
-		while(it.hasNext())
-		{
-			try {
-				DataOutputStream out = 
-						(DataOutputStream)clients.get(it.next());
-				System.out.println(msg);
+   HashMap<String, DataOutputStream>clients ;
+   
+   public Server() {
+      //  이거로 바껴랏
+      clients =new HashMap();
+      Collections.synchronizedMap(clients);
+      SimpleDateFormat sf = new SimpleDateFormat();
+      
+      try {
+         ServerSocket server = new ServerSocket(7777);
+         System.out.println("["+sf.format(new Date())+"]"+"서버 시작");
+         
+         while(true)
+         {
+            Socket client = server.accept();
+            System.out.println("["+sf.format(new Date())+"]"+
+                  "["+"로그인"+":"+client.getPort()+"] 에서 접속");
+            
+            new Receiver(client).start();
+         }
+         
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+   }
+   
+   class Receiver extends Thread
+   {
+      String name;
+      DataOutputStream output;
+      DataInputStream input;
+      public Receiver(Socket client) {
+         try {
+            name = "["+client.getInetAddress()+"]";
+            output = new DataOutputStream(client.getOutputStream());
+            input = new DataInputStream(client.getInputStream());
+               
+         } catch (IOException e) {
+            e.printStackTrace();
+         }
+      }
+      
+      @Override
+      public void run() {
+         
+         try {
+            name = input.readUTF();
+            sendToAll("#"+name+"님이 입장하였습니다.");
+            clients.put(name, output);
+            
+            System.out.println("현재접속자수:"+clients.size());
+            
+            while(input!=null)
+            {
+               sendToAll(input.readUTF());
+               System.out.println(input.readUTF());
+            }
+         } catch (IOException e) {
+            clients.remove(name);
+            sendToAll("#"+name+"님이 퇴장하셨습니다.");
+            System.out.println("현재접속자수:"+clients.size());
+         }
+      
+      }// run 종료
+   }
+   void sendToAll(String msg)
+   {
+      Iterator it =clients.keySet().iterator();
+      while(it.hasNext())
+      {
+         try {
+            DataOutputStream out = 
+                  (DataOutputStream)clients.get(it.next());
+            System.out.println(msg);
 
-				out.writeUTF(msg);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-	public static void main(String[] args) {
-		new Server();
-	}
+            out.writeUTF(msg);
+         } catch (IOException e) {
+            e.printStackTrace();
+         }
+      }
+   }
+   public static void main(String[] args) {
+      new Server();
+   }
 }
